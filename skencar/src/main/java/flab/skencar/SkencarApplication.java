cb9f025e116b4a3833a02d7be12b642415c43a0d
JunWon_Lee <99373291+Gomdollee@users.noreply.github.com>
2025-01-19 16:09:07 +0900

    Add files via upload

---
+++ skencar/src/main/java/flab/skencar/SkencarApplication.java
@@ -0,0 +1,13 @@
+package flab.skencar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkencarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkencarApplication.class, args);
+	}
+
+}
---
+++ skencar/src/test/java/flab/skencar/SkencarApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.skencar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkencarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
